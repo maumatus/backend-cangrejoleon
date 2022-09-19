b330120e027ef61113e1a1e648c0479df299d128
Mauricio Matus F <mauricio@Mauricios-MacBook-Pro.local>
2022-09-19 10:56:14 -0300

    Commit02: ahora es funcional controlador de peticion GET a BD simulada. Falta crear el modelo de datos y la creación de la base de datos

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+//Conexión DB
+const mongoose = require("mongoose");
+
+/*-------DB--------*/
+//Seteamos la conexión al ORM Mongoose
+const mongoDB = "mongodb://127.0.0.1/my_database";
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Obtenemos la conexión por defecto
+const db = mongoose.connection;
+
+//Unimos la conexión a evento error (para obtener notificaciones de errores)
+db.on("error", console.error.bind(console, "MongoDB conección error:"));
+/*-------DB--------*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Route handling middlewares
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var router = express.Router();
-const books = require('../bd/books.js')
+const express = require('express');
+const router = express.Router();
+const data = require('../bd/books.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', {
     title: "Titulo prueba",
-    data: books
+    data: data,
   });
+  console.log("vemos si nos carga los libros en este scope")
+  console.log(data)
 });
 
 
